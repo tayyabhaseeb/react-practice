71f63f66c5f6c9f787fec36019971d526f357062
Tayyab Haseeb <73613576+tayyabhaseeb@users.noreply.github.com>
2023-11-26 23:58:12 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Button from "./Button";
+import Card from "./card";
+import Gallery from "./Gallery";
+import Header from "./Header";
+
+export default function App() {
+  return (
+    <div>
+      <Header />
+      <Card />
+      <Gallery />
+      <Button />
+    </div>
+  );
+}
